35e426356c5d654c2714c3de568c8428d8a0a73d
vercellone <jason.vercellone@gmail.com>
2023-04-12 10:43:20 -0500

    check "path"

--- test/sfdx-cli/test.sh
+++ test/sfdx-cli/test.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "version" bash -c "echo $PATH | grep 'sfdx'"
+check "path" bash -c "echo $PATH | grep 'sfdx'"
 check "version" bash -c "sfdx version | grep 'sfdx-cli/'"
 
 # Report result
